9d135fde511598be08c9e9a7716d3f28ad143da0
SammyMwaura <mwaurasammy500@gmail.com>
2024-07-05 23:23:07 +0300

    array map method

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial.replace(/\b\w/g, l => l.toUpperCase())
+  });
+};
